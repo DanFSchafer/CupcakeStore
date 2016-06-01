70ba45ddacc145d1c89a48fef25efc7d51b7e28c
Dan Schafer <danfschafer@gmail.com>
2016-05-31 20:36:59 -0700

    Add some exercise code from DOM manipulation to highlight the paragraphs when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(function() {
   $("p").click(function() {
     $(this).addClass("class1");
+    $("p.class1").click(function() {
+      $(this).removeClass("class1");
+  });
   });
 });
 //  $(".clickable").click(function() {
